a79b5849456c18767675476b62be97a29e71d0ce
Andrew Minion <andrew@andrewrminion.com>
2014-07-16 17:38:55 -0400

    menu - add slight shadow when scrolled
    use stickUp to add slight shadow when page is partially scrolled

--- footer.php
+++ footer.php
@@ -24,7 +24,7 @@
 <script type="text/javascript">
   jQuery(function($) {
     $(document).ready( function() {
-      $('.main-navigation').stickUp();
+      $('.site-header').stickUp();
     });
   });
 
