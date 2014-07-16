3ec0048b0ab22c2283d21807caacedb46b596246
Andrew Minion <andrew@andrewrminion.com>
2014-07-16 17:26:07 -0400

    menu - fix for small screens
    tweak height
    move and relabel button

--- header.php
+++ header.php
@@ -24,8 +24,8 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle"><?php _e( 'Primary Menu', 'ebckm-2014' ); ?></button>
             <a class="site-logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="<?php echo get_template_directory_uri(); ?>/images/EBC-logo.svg" alt="Emmanuel Baptist Church Logo" title="Emmanuel Baptist Church" /></a>
+			<button class="menu-toggle"><?php _e( 'Main Menu', 'ebckm-2014' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
