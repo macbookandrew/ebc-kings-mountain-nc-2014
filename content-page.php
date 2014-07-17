c8d553e5095c25c3a7e7435acca0e6c48413e044
Andrew Minion <andrew@andrewrminion.com>
2014-07-17 08:59:02 -0400

    featured images - add support for large image
    add page_header image size
    add page_header to page

--- content-page.php
+++ content-page.php
@@ -6,6 +6,11 @@
  */
 ?>
 
+<?php
+if ( has_post_thumbnail() ) {
+    the_post_thumbnail( 'page_header' );
+}
+?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
