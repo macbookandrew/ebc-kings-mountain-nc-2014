6688c80da5e90475ca4eb83a6e78f2b18f54cfd3
Andrew Minion <andrew@andrewrminion.com>
2015-05-23 14:37:06 -0400

    tweak 404 form

--- 404.php
+++ 404.php
@@ -1,57 +1,38 @@
 <?php
 /**
- * The template for displaying 404 pages (not found).
+ * Modified page.php template
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
  *
  * @package EBC Kings Mountain, NC
  */
 
 get_header(); ?>
 
-<div id="content" class="site-content">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'ebckm-2014' ); ?></h1>
-				</header><!-- .page-header -->
+<div id="content" class="site-content no-thumbnail">
+<div id="primary" class="content-area">
+<main id="main" class="site-main" role="main">
 
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'ebckm-2014' ); ?></p>
+<article class="error-404 not-found">
+	<header class="entry-header" aria-hidden="false">
+        <h1 class="entry-title">Not Found</h1>
+	</header><!-- .entry-header -->
 
-					<?php get_search_form(); ?>
+	<div class="entry-content">
+        <p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'ebckm-2014' ); ?></p>
 
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+        <?php get_search_form(); ?>
 
-					<?php if ( ebckm_2014_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php _e( 'Most Used Categories', 'ebckm-2014' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-					<?php endif; ?>
+	</div><!-- .entry-content -->
 
-					<?php
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'ebckm-2014' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
-
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
+</article><!-- #post-## -->
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
